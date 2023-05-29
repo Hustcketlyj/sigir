514fcdad61b2ba5f7a3077b2addeca2b728cb6b7
Hustcketlyj <40982662+Hustcketlyj@users.noreply.github.com>
2023-05-29 12:41:56 +0800

    Create QA_preprocess.py

---
+++ QA_preprocess.py
@@ -0,0 +1,15 @@
+import jsonlines
+import json
+
+
+for i in range(34):
+  f=jsonlines.open('low_resource_LR10_'+str(i)+'_DA_allen.jsonl')
+  data=[line for line in f.iter()]
+  for item in data:
+    item['context'] = item.pop('LM')
+  #print(data[:2])
+  json_object = json.dumps(data)
+  new_filename='low_resource_LR10_'+str(i)+'_QA.json'
+  with open(new_filename, "w") as outfile:
+    outfile.write(json_object)
+  outfile.close()
